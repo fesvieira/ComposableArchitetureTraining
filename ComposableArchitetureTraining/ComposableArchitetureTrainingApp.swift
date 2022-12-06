be0ef70f22a9ae22ffc540051a0866ba297024ed
felipevieira <felipe2204sv@gmail.com>
2022-12-06 08:33:28 -0300

    create project

---
+++ ComposableArchitetureTraining/ComposableArchitetureTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitetureTrainingApp.swift
+//  ComposableArchitetureTraining
+//
+//  Created by Felipe Vieira on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitetureTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
